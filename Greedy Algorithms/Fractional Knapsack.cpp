ce12f7bc90a12d96c14245817f0d64687fad7ec4
Raian Rashid <rimonraian2@gmail.com>
2025-01-12 13:27:00 +0600

    fractionalknapsack.cpp

--- Greedy Algorithms/Fractional Knapsack.cpp
+++ Greedy Algorithms/Fractional Knapsack.cpp
@@ -1,58 +1,74 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+
 using namespace std;
-struct Item
-{
+
+struct Item {
     float v;
     float w;
     float d;
 };
-bool compare(Item i1, Item i2)
-{
-    if(i1.d > i2.d)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+
+bool compare(Item i1, Item i2) {
+    return i1.d > i2.d;
 }
-int main()
-{
-    Item items[100];
+
+int main() {
     int W;
-    cout<< "Enter Knapsack weight \n";
+    cout << "Enter Knapsack weight: ";
     cin >> W;
-    int n;
-    cout << "Enter the amount of items"<<"\n";
-    cin >>n;
-    for(int i=0; i<n; i++)
-    {
-        cout << "Enter Weight "<< i+1<<"\n";
-        cin >> items[i].w;
-        cout << "Enter Value "<< i+1<<"\n";
-        cin >> items[i].v;
+
+    if (W <= 0) {
+        cout << "Knapsack weight must be a positive value!" << endl;
+        return 1;
     }
-    for (int i=0; i<n; i++)
-    {
+
+    int n;
+    cout << "Enter the number of items: ";
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "Number of items must be positive!" << endl;
+        return 1;
+    }
+
+    vector<Item> items(n);
+
+    for (int i = 0; i < n; i++) {
+        cout << "Enter Weight of item " << i + 1 << ": ";
+        cin >> items[i].w;
+        if (items[i].w <= 0) {
+            cout << "Weight must be positive!" << endl;
+            return 1;
+        }
+        cout << "Enter Value of item " << i + 1 << ": ";
+        cin >> items[i].v;
+        if (items[i].v <= 0) {
+            cout << "Value must be positive!" << endl;
+            return 1;
+        }
         items[i].d = items[i].v / items[i].w;
     }
-    sort(items, items+n, compare);
+
+    sort(items.begin(), items.end(), compare);
+
     float totalValue = 0, totalWeight = 0;
-     for(int i=0; i<n; i++)
-    {
-        if(totalWeight + items[i].w<= W)
-        {
-            totalValue += items[i].v ;
+
+    for (int i = 0; i < n; i++) {
+        if (totalWeight + items[i].w <= W) {
+            totalValue += items[i].v;
             totalWeight += items[i].w;
-        }
-        else
-        {
-            int wt = W-totalWeight;
-            totalValue += (wt * items[i].d);
+        } else {
+            int wt = W - totalWeight;
+            totalValue += wt * items[i].d;
             totalWeight += wt;
             break;
         }
     }
-    cout << "Max profit possible is "<< totalValue;
+
+    cout << fixed << setprecision(2) << "Max profit possible is " << totalValue << endl;
+
+    return 0;
 }
