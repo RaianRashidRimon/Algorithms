a2104cf75386b2c98243b2d96172c7b220c3de88
Raian Rashid <rimonraian2@gmail.com>
2025-01-15 02:14:08 +0600

    merge sort

--- Sorting and Searching/Merge Sort.cpp
+++ Sorting and Searching/Merge Sort.cpp
@@ -5,54 +5,72 @@ void merge(int arr[], int p, int q, int r)
 {
     int n1 = q - p + 1;
     int n2 = r - q;
-    int L[n1], M[n2];
+    int* L = new int[n1];
+    int* M = new int[n2];
     for (int i = 0; i < n1; i++)
         L[i] = arr[p + i];
     for (int j = 0; j < n2; j++)
         M[j] = arr[q + 1 + j];
-    int i, j, k;
-    i = 0;
-    j = 0;
-    k = p;
-    while (i < n1 && j < n2){
-        if (L[i] <= M[j]){
+    int i = 0, j = 0, k = p;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= M[j])
+        {
             arr[k] = L[i];
-            i++;}
-        else{
+            i++;
+        }
+        else
+        {
             arr[k] = M[j];
-            j++;}
-        k++;}
-    while (i < n1){
+            j++;
+        }
+        k++;
+    }
+    while (i < n1)
+    {
         arr[k] = L[i];
         i++;
-        k++;}
-    while (j < n2){
+        k++;
+    }
+    while (j < n2)
+    {
         arr[k] = M[j];
         j++;
-        k++;}
+        k++;
+    }
+    delete[] L;
+    delete[] M;
 }
 void mergeSort(int arr[], int l, int r)
 {
-    if (l < r){
+    if (l < r)
+    {
         int m = l + (r - l) / 2;
         mergeSort(arr, l, m);
         mergeSort(arr, m + 1, r);
-        merge(arr, l, m, r);}
+        merge(arr, l, m, r);
+    }
 }
-
 void printArray(int arr[], int size)
 {
     for (int i = 0; i < size; i++)
         cout << arr[i] << " ";
     cout << endl;
 }
-
 int main()
 {
-    int arr[] = {6, 5, 12, 10, 9, 1};
-    int size = sizeof(arr) / sizeof(arr[0]);
+    int size;
+    cout << "Enter the number of elements in the array: ";
+    cin >> size;
+    int* arr = new int[size];
+    cout << "Enter the elements of the array: " << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
     mergeSort(arr, 0, size - 1);
     cout << "Sorted array: \n";
     printArray(arr, size);
+    delete[] arr;
     return 0;
 }
