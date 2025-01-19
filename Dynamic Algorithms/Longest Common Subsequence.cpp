be88a90a03c602b7597c0a8c20bcffcd0bce8dc9
Raian Rashid <rimonraian2@gmail.com>
2025-01-20 01:55:04 +0600

    lcs

--- Dynamic Algorithms/Longest Common Subsequence.cpp
+++ Dynamic Algorithms/Longest Common Subsequence.cpp
@@ -1,21 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-int lcs( char *X, char *Y, int m, int n )
+
+int lcs(string X, string Y, int m, int n)
 {
     if (m == 0 || n == 0)
         return 0;
-    if (X[m-1] == Y[n-1])
-        return 1 + lcs(X, Y, m-1, n-1);
+    if (X[m - 1] == Y[n - 1])
+        return 1 + lcs(X, Y, m - 1, n - 1);
     else
-        return max(lcs(X, Y, m, n-1), lcs(X, Y, m-1, n));
+        return max(lcs(X, Y, m, n - 1), lcs(X, Y, m - 1, n));
 }
-
 int main()
 {
-    char X[] = "GXTXAYB";
-    char Y[] = "AGGTAB";
-    int m = strlen(X);
-    int n = strlen(Y);
-    cout<<"Length of LCS is "<< lcs( X, Y, m, n ) ;
+    string X, Y;
+    cout << "Enter the first string: ";
+    cin >> X;
+    cout << "Enter the second string: ";
+    cin >> Y;
+    int m = X.length();
+    in n = Y.length();
+    cout << "Length of LCS is " << lcs(X, Y, m, n) << endl;
     return 0;
 }
