f607478e5e49ed8692f11ad903a4950bea4ede91
Raian Rashid <rimonraian2@gmail.com>
2025-01-20 01:52:35 +0600

    0-1 Knapsack

--- Dynamic Algorithms/0-1 Knapsack.cpp
+++ Dynamic Algorithms/0-1 Knapsack.cpp
@@ -1,25 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int max(int a, int b) {
-    return (a > b) ? a : b;
+int knapSack(int W, int wt[], int val[], int n) {
+    vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
+
+    for (int i = 1; i <= n; i++) {
+        for (int w = 0; w <= W; w++) {
+            if (wt[i - 1] <= w)
+                dp[i][w] = max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w]);
+            else
+                dp[i][w] = dp[i - 1][w];
+        }
     }
 
-int knapSack(int W, int wt[], int val[], int n)
-{
-    if (n == 0 || W == 0)
-        return 0;
-    if (wt[n - 1] > W)
-        return knapSack(W, wt, val, n - 1);
-    else
-        return max(
-            val[n - 1]
-                + knapSack(W - wt[n - 1],
-                           wt, val, n - 1),
-            knapSack(W, wt, val, n - 1));
+    return dp[n][W];
 }
-int main()
-{
+
+int main() {
     int val[] = { 60, 100, 120 };
     int wt[] = { 10, 20, 30 };
     int W = 50;
