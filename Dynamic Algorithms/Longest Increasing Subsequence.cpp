4b5c572d21f0ebdf476a2451e9ede0dc83d19502
Raian Rashid <rimonraian2@gmail.com>
2025-01-21 01:15:40 +0600

    lis

--- Dynamic Algorithms/Longest Increasing Subsequence.cpp
+++ Dynamic Algorithms/Longest Increasing Subsequence.cpp
@@ -2,26 +2,29 @@
 #include <climits>
 using namespace std;
 
-int LIS(int arr[], int i, int n, int prev) {
-    if (i == n) {
+int LIS(int arr[], int i, int n, int prev)
+{
+    if (i == n)
+    {
         return 0;
     }
     int excl = LIS(arr, i + 1, n, prev);
     int incl = 0;
-    if (arr[i] > prev) {
+    if (arr[i] > prev)
+    {
         incl = 1 + LIS(arr, i + 1, n, arr[i]);
     }
     return max(incl, excl);
 }
-
-int main() {
+int main()
+{
     int n;
     cout << "Enter the size of the array: ";
     cin >> n;
-
     int arr[n];
     cout << "Enter the elements of the array: ";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cin >> arr[i];
     }
     cout << "The length of the LIS is " << LIS(arr, 0, n, INT_MIN) << endl;
