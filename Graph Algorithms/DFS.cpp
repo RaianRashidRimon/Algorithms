ee0960c5773e08dee0cf7cdad7da305acc141c83
Raian Rashid <rimonraian2@gmail.com>
2025-01-22 01:20:07 +0600

    dfs

--- Graph Algorithms/DFS.cpp
+++ Graph Algorithms/DFS.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Graph {
+class Graph
+{
 public:
     map<int, bool> visited;
     map<int, list<int>> adj;
@@ -9,12 +10,13 @@ public:
     void addEdge(int v, int w);
     void DFS(int v);
 };
-
-void Graph::addEdge(int v, int w) {
+void Graph::addEdge(int v, int w)
+{
     adj[v].push_back(w);
 }
 
-void Graph::DFS(int v) {
+void Graph::DFS(int v)
+{
     visited[v] = true;
     cout << v << " ";
 
@@ -22,27 +24,23 @@ void Graph::DFS(int v) {
         if (!visited[*i])
             DFS(*i);
 }
-
-int main() {
+int main()
+{
     Graph g;
-
     int numEdges;
     cout << "Enter the number of edges: ";
     cin >> numEdges;
-
     cout << "Enter the edges (source destination):" << endl;
-    for (int i = 0; i < numEdges; i++) {
+    for (int i = 0; i < numEdges; i++)
+    {
         int v, w;
         cin >> v >> w;
         g.addEdge(v, w);
     }
-
     int startVertex;
     cout << "Enter the starting vertex for DFS: ";
     cin >> startVertex;
-
     cout << "Following is Depth First Traversal (starting from vertex " << startVertex << "):" << endl;
     g.DFS(startVertex);
-
     return 0;
 }
