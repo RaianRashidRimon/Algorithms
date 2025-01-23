4b025c3d7985dda8190ba55efcb9bbb48c4067cf
Raian Rashid <rimonraian2@gmail.com>
2025-01-23 22:18:39 +0600

    bfs

--- Graph Algorithms/BFS.cpp
+++ Graph Algorithms/BFS.cpp
@@ -1,52 +1,74 @@
-#include<iostream>
+#include <iostream>
 #include <list>
 using namespace std;
-class Graph{
-    int V;
-    list<int> *adj;
+
+class Graph
+{
+    int V;                  
+    list<int>* adj;         
 public:
     Graph(int V);
     void addEdge(int v, int w);
     void BFS(int s);
 };
-Graph::Graph(int V){
+
+Graph::Graph(int V)
+{
     this->V = V;
     adj = new list<int>[V];
 }
-void Graph::addEdge(int v, int w){
+
+void Graph::addEdge(int v, int w)
+{
     adj[v].push_back(w);
 }
-void Graph::BFS(int s){
-    bool *visited = new bool[V];
-    for(int i = 0; i < V; i++)
+
+void Graph::BFS(int s)
+{
+    bool* visited = new bool[V];
+    for (int i = 0; i < V; i++)
         visited[i] = false;
     list<int> queue;
     visited[s] = true;
     queue.push_back(s);
     list<int>::iterator i;
-while(!queue.empty()){
+    while (!queue.empty())
+    {
         s = queue.front();
         cout << s << " ";
         queue.pop_front();
-        for (i = adj[s].begin(); i != adj[s].end(); ++i){
-            if (!visited[*i]){
+
+        for (i = adj[s].begin(); i != adj[s].end(); ++i)
+        {
+            if (!visited[*i])
+            {
                 visited[*i] = true;
-                queue.push_back(*i);}
+                queue.push_back(*i);
+            }
         }
     }
-}
-int main()
-{
-    Graph g(4);
-    g.addEdge(0, 1);
-    g.addEdge(0, 2);
-    g.addEdge(1, 2);
-    g.addEdge(2, 0);
-    g.addEdge(2, 3);
-    g.addEdge(3, 3);
-    cout << "Breadth First Traversal "
-         << "(starting from vertex 2) \n";
-    g.BFS(2);
-    return 0;
+    delete[] visited;
 }
 
+int main()
+{
+    int V, E;
+    cout << "Enter the number of vertices: ";
+    cin >> V;
+    Graph g(V);
+    cout << "Enter the number of edges: ";
+    cin >> E;
+    cout << "Enter the edges (source destination):" << endl;
+    for (int i = 0; i < E; i++)
+    {
+        int v, w;
+        cin >> v >> w;
+        g.addEdge(v, w);
+    }
+    int startVertex;
+    cout << "Enter the starting vertex for BFS: ";
+    cin >> startVertex;
+    cout << "Breadth First Traversal (starting from vertex " << startVertex << "):" << endl;
+    g.BFS(startVertex);
+    return 0;
+}
