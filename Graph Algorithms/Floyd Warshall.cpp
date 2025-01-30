609a3d8101d0e68a579dfb7a2510487a771468d2
Raian Rashid <rimonraian2@gmail.com>
2025-01-30 12:23:40 +0600

    floydwarshall

--- Graph Algorithms/Floyd Warshall.cpp
+++ Graph Algorithms/Floyd Warshall.cpp
@@ -1,50 +1,55 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
-#define V 4
 #define INF 99999
 
-void printSolution(int dist[][V]);
-void floydWarshall(int graph[][V])
+void printSolution(vector<vector<int> >& dist, int V);
+void floydWarshall(vector<vector<int> >& graph, int V)
 {
-    int dist[V][V], i, j, k;
-    for (i = 0; i < V; i++)
-        for (j = 0; j < V; j++)
-            dist[i][j] = graph[i][j];
-    for (k = 0; k < V; k++) {
-        for (i = 0; i < V; i++) {
-            for (j = 0; j < V; j++) {
-                if (dist[i][j] > (dist[i][k] + dist[k][j])
-                    && (dist[k][j] != INF
-                        && dist[i][k] != INF))
+    vector<vector<int> > dist = graph;
+    for (int k = 0; k < V; k++)
+    {
+        for (int i = 0; i < V; i++)
+        {
+            for (int j = 0; j < V; j++)
+            {
+                if (dist[i][k] != INF && dist[k][j] != INF &&
+                        dist[i][j] > dist[i][k] + dist[k][j])
                     dist[i][j] = dist[i][k] + dist[k][j];
             }
         }
     }
-    printSolution(dist);
+    printSolution(dist, V);
 }
-
-void printSolution(int dist[][V])
+void printSolution(vector<vector<int> >& dist, int V)
 {
-    cout << "The following matrix shows the shortest "
-            "distances"
-            " between every pair of vertices \n";
-    for (int i = 0; i < V; i++) {
-        for (int j = 0; j < V; j++) {
+    cout << "The shortest distances between every pair of vertices:\n";
+    for (int i = 0; i < V; i++)
+    {
+        for (int j = 0; j < V; j++)
+        {
             if (dist[i][j] == INF)
-                cout << "INF"
-                     << "     ";
+                cout << "INF\t";
             else
-                cout << dist[i][j] << "     ";
+                cout << dist[i][j] << "\t";
         }
         cout << endl;
     }
 }
 int main()
 {
-    int graph[V][V] = { { 0, 5, INF, 10 },
-                        { INF, 0, 3, INF },
-                        { INF, INF, 0, 1 },
-                        { INF, INF, INF, 0 } };
-    floydWarshall(graph);
+    int V;
+    cout << "Enter the number of vertices: ";
+    cin >> V;
+    vector<vector<int> > graph(V, vector<int>(V));
+    cout << "Enter the adjacency matrix (use " << INF << " for infinity):\n";
+    for (int i = 0; i < V; i++)
+    {
+        for (int j = 0; j < V; j++)
+        {
+            cin >> graph[i][j];
+        }
+    }
+    floydWarshall(graph, V);
     return 0;
 }
