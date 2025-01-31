6d92cbed63d6b8975549920099f6239831367d7f
Raian Rashid <rimonraian2@gmail.com>
2025-02-01 00:43:02 +0600

    prims

--- Graph Algorithms/Prims.cpp
+++ Graph Algorithms/Prims.cpp
@@ -1,55 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define V 5
 
-int minKey(int key[], bool mstSet[])
-{
-
-    int min = INT_MAX, min_index;
-
-    for (int v = 0; v < V; v++)
-        if (mstSet[v] == false && key[v] < min)
-            min = key[v], min_index = v;
-
-    return min_index;
-}
-void printMST(int parent[], int graph[V][V])
+#define INF INT_MAX
+void printMST(vector<int>& parent, vector<vector<int> >& graph, int V)
 {
     cout << "Edge \tWeight\n";
     for (int i = 1; i < V; i++)
-        cout << parent[i] << " - " << i << " \t"
-             << graph[i][parent[i]] << " \n";
+        cout << parent[i] << " - " << i << " \t" << graph[i][parent[i]] << " \n";
 }
-
-void primMST(int graph[V][V])
+int minKey(vector<int>& key, vector<bool>& mstSet, int V)
 {
-    int parent[V];
-    int key[V];
-    bool mstSet[V];
-    for (int i = 0; i < V; i++)
-        key[i] = INT_MAX, mstSet[i] = false;
+    int min = INF, min_index;
+    for (int v = 0; v < V; v++)
+        if (!mstSet[v] && key[v] < min)
+            min = key[v], min_index = v;
+    return min_index;
+}
+void primMST(vector<vector<int> >& graph, int V)
+{
+    vector<int> parent(V);
+    vector<int> key(V, INF);
+    vector<bool> mstSet(V, false);
     key[0] = 0;
     parent[0] = -1;
-    for (int count = 0; count < V - 1; count++) {
-        int u = minKey(key, mstSet);
+    for (int count = 0; count < V - 1; count++)
+    {
+        int u = minKey(key, mstSet, V);
         mstSet[u] = true;
+
         for (int v = 0; v < V; v++)
-            if (graph[u][v] && mstSet[v] == false
-                && graph[u][v] < key[v])
+            if (graph[u][v] && !mstSet[v] && graph[u][v] < key[v])
                 parent[v] = u, key[v] = graph[u][v];
     }
-    printMST(parent, graph);
+    printMST(parent, graph, V);
 }
-
-
 int main()
 {
-    int graph[V][V] = { { 0, 2, 0, 6, 0 },
-                        { 2, 0, 3, 8, 5 },
-                        { 0, 3, 0, 0, 7 },
-                        { 6, 8, 0, 0, 9 },
-                        { 0, 5, 7, 9, 0 } };
-    primMST(graph);
-
+    int V;
+    cout << "Enter number of vertices: ";
+    cin >> V;
+    vector<vector<int> > graph(V, vector<int>(V, 0));
+    cout << "Enter adjacency matrix:\n";
+    for (int i = 0; i < V; i++)
+        for (int j = 0; j < V; j++)
+            cin >> graph[i][j];
+    cout << "\nMinimum Spanning Tree (MST) using Prim's Algorithm:\n";
+    primMST(graph, V);
     return 0;
 }
