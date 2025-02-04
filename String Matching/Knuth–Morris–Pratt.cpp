6c1773387c588397621bcd8d73b526c71e7c76e9
Raian Rashid <rimonraian2@gmail.com>
2025-02-04 22:44:45 +0600

    kmp

--- String Matching/Knuth–Morris–Pratt.cpp
+++ String Matching/Knuth–Morris–Pratt.cpp
@@ -1,45 +1,75 @@
 #include <iostream>
+#include <vector>
 #include <cstring>
 using namespace std;
-void buildLPS(char* pattern, int* lps, int m) {
+
+void buildLPS(const char* pattern, vector<int>& lps, int m)
+{
     int len = 0;
     lps[0] = 0;
     int i = 1;
-    while (i < m) {
-        if (pattern[i] == pattern[len]) {
+    while (i < m)
+    {
+        if (pattern[i] == pattern[len])
+        {
             len++;
             lps[i] = len;
-            i++;}
-        else {
-            if (len != 0) {
-                len = lps[len - 1];}
-            else {
+            i++;
+        }
+        else
+        {
+            if (len != 0)
+            {
+                len = lps[len - 1];
+            }
+            else
+            {
                 lps[i] = 0;
-                i++;}}}}
-void KMP(char* text, char* pattern) {
+                i++;
+            }
+        }
+    }
+}
+void KMP(const char* text, const char* pattern)
+{
     int n = strlen(text);
     int m = strlen(pattern);
-    int lps[m];
+    vector<int> lps(m);
     buildLPS(pattern, lps, m);
-    int i = 0;
-    int j = 0;
-    while (i < n) {
-        if (pattern[j] == text[i]) {
+    int i = 0, j = 0;
+    while (i < n)
+    {
+        if (pattern[j] == text[i])
+        {
             j++;
-            i++;}
-        if (j == m) {
-            cout << "Found pattern at index " << i - j << endl;
+            i++;
+        }
+
+        if (j == m)
+        {
+            cout << "Pattern found at index " << i - j << endl;
             j = lps[j - 1];
         }
-        else if (i < n && pattern[j] != text[i]) {
-            if (j != 0) {
+        else if (i < n && pattern[j] != text[i])
+        {
+            if (j != 0)
+            {
                 j = lps[j - 1];
             }
-            else {
-                i++;}}}}
-int main() {
-    char text[] = "ABCABCABCABCD";
-    char pattern[] = "ABCABCD";
-    KMP(text, pattern);
-    return 0;}
-
+            else
+            {
+                i++;
+            }
+        }
+    }
+}
+int main()
+{
+    string text, pattern;
+    cout << "Enter text: ";
+    cin >> text;
+    cout << "Enter pattern: ";
+    cin >> pattern;
+    KMP(text.c_str(), pattern.c_str());
+    return 0;
+}
