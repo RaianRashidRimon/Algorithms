627f62840cb3f48aff5af3640f728b46d01c9900
Raian Rashid <rimonraian2@gmail.com>
2025-02-07 00:49:28 +0600

    Update Square Root Decomposition.cpp

--- Advanced Data Structures/Square Root Decomposition.cpp
+++ Advanced Data Structures/Square Root Decomposition.cpp
@@ -1,32 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 const int MAXN = 100005;
 int arr[MAXN], block[MAXN], blk_sz;
-void build(int n) {
+
+void build(int n)
+{
     blk_sz = sqrt(n);
-    for (int i = 0; i < n; i++) {
-        block[i/blk_sz] += arr[i];
+    memset(block, 0, sizeof(block));
+    for (int i = 0; i < n; i++)
+    {
+        block[i / blk_sz] += arr[i];
     }
 }
-int query(int l, int r) {
+int query(int l, int r)
+{
     int sum = 0;
-    while (l < r and l%blk_sz != 0 and l != 0) {
-        sum += arr[l++];}
-    while (l+blk_sz <= r) {
-        sum += block[l/blk_sz];
-        l += blk_sz;}
-    while (l <= r) {
-        sum += arr[l++];}
+    while (l <= r && l % blk_sz != 0)
+    {
+        sum += arr[l++];
+    }
+    while (l + blk_sz - 1 <= r)
+    {
+        sum += block[l / blk_sz];
+        l += blk_sz;
+    }
+    while (l <= r)
+    {
+        sum += arr[l++];
+    }
     return sum;
 }
-int main() {
+
+int main()
+{
     int n;
+    cout << "Enter number of elements: ";
     cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];}
+    cout << "Enter " << n << " elements:\n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
     build(n);
-    int l, r;
-    cin >> l >> r;
-    cout << query(l, r) << endl;
+    int q;
+    cout << "Enter number of queries: ";
+    cin >> q;
+    while (q--)
+    {
+        int l, r;
+        cout << "Enter range (l r): ";
+        cin >> l >> r;
+        cout << "Sum in range [" << l << ", " << r << "] = " << query(l, r) << endl;
+    }
     return 0;
 }
