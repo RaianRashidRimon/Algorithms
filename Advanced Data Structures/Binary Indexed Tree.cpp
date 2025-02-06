3bf1013a564be9d13affb9ae2ed86a52381e21c3
Raian Rashid <rimonraian2@gmail.com>
2025-02-07 00:45:14 +0600

    BIT

--- Advanced Data Structures/Binary Indexed Tree.cpp
+++ Advanced Data Structures/Binary Indexed Tree.cpp
@@ -1,30 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 const int MAXN = 100005;
 int BIT[MAXN];
 int n;
-void update(int idx, int val) {
-    while (idx <= n) {
+
+void update(int idx, int val)
+{
+    while (idx <= n)
+    {
         BIT[idx] += val;
         idx += (idx & -idx);
     }
 }
-int query(int idx) {
+int query(int idx)
+{
     int sum = 0;
-    while (idx > 0) {
+    while (idx > 0)
+    {
         sum += BIT[idx];
         idx -= (idx & -idx);
     }
     return sum;
 }
-int main() {
+int rangeQuery(int l, int r)
+{
+    return query(r) - query(l - 1);
+}
+
+int main()
+{
+    cout << "Enter number of elements: ";
     cin >> n;
-    for (int i = 1; i <= n; i++) {
+    cout << "Enter " << n << " elements:\n";
+    for (int i = 1; i <= n; i++)
+    {
         int x;
         cin >> x;
-        update(i, x);}
+        update(i, x);
+    }
+    int q;
+    cout << "Enter number of queries: ";
+    cin >> q;
+    while (q--)
+    {
         int l, r;
+        cout << "Enter range (l r): ";
         cin >> l >> r;
-        cout << query(r) - query(l-1) << endl;
+        cout << "Sum in range [" << l << ", " << r << "] = " << rangeQuery(l, r) << endl;
+    }
     return 0;
 }
